6f06e9a252b4de2a16a131c9cfb9277e922b8b9e
Michael Hanson <michael.b.hanson@gmail.com>
2012-06-30 10:37:41 -0700

    Change function name to 'create'

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io])
   (:import java.net.Socket))
 
-(defn create-connection [host port]
+(defn create [host port]
   (let [socket (Socket. host port)]
     {:socket socket
      :in (io/reader socket)
