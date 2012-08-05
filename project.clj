e6f05dacd6e68271eb9869a2418563653fc17626
Michael Hanson <michael.b.hanson@gmail.com>
2012-08-05 14:10:14 -0700

    Add a description

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject pgbot "0.0.1"
+  :description "A simple IRC bot, written in Clojure."
   :license {:name "The MIT License"
             :url "http://www.opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
