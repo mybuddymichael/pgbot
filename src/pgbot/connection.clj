622a881686105785cb087c119cd51a9660dc285a
Michael Hanson <michael.b.hanson@gmail.com>
2012-08-05 14:59:04 -0700

    Add the send-message and register commands

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -10,3 +10,14 @@
     {:socket socket
      :in (io/reader socket)
      :out (io/writer socket)}))
+
+(defn send-message [irc message]
+  (binding [*out* (irc :out)]
+    (println message)))
+
+(defn register [irc nickname & [password]]
+  (send-message irc (str "NICK " nickname))
+  (send-message irc (str "USER " nickname " i * " nickname))
+  (when password
+    (send-message
+      irc (str "PRIVMSG nickserv :identify" nickname password))))
