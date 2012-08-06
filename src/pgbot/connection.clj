04acf17ca980eeb12869066a156742cdd3940693
Michael Hanson <michael.b.hanson@gmail.com>
2012-08-05 21:56:36 -0700

    Add a missing space

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -4,7 +4,7 @@
 
 (defn create
   "Given a host and a port, generate an IRC connection map, containing
-  pairs for the socket, the reader, and the writer."
+   pairs for the socket, the reader, and the writer."
   [host port]
   (let [socket (Socket. host (Integer. port))]
     {:socket socket
