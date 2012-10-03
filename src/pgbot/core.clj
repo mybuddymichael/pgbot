b3cb9aeddceb868bcda54f0a240269376fe6306f
Michael Hanson <michael.b.hanson@gmail.com>
2012-10-03 15:51:28 -0700

    Have "-main" use "connect"

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,12 +1,6 @@
 (ns pgbot.core
   (:require [pgbot.connection :as connection]))
 
-(defn -main [host port nickname & [password channel]]
-  (while true
-    (let [irc (connection/create host port)]
-      (if password
-        (connection/register irc nickname password)
-        (connection/register irc nickname))
-      (when channel
-        (connection/join irc channel))
-      (connection/print-input irc))))
+(defn -main [host port nick & [password channel]]
+  (connection/connect :host host :port port :nick nick
+                      :password password :channel channel))
