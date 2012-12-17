c15f059d575f5623aabf3823559dc2413570f773
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-17 15:16:31 -0800

    Clear out pgbot.connection

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -1,8 +1,7 @@
 (ns pgbot.connection
   "A collection of functions for creating and maintaining connections to
   IRC servers."
-  (:require (clojure [string :as string])
-            (clojure.java [io :as io]))
+  (:require (clojure.java [io :as io]))
   (:import java.net.Socket))
 
 (defn create
@@ -13,27 +12,3 @@
      :in (io/reader socket)
      :out (io/writer socket)
      :nick nick}))
-
-(defn send-message [connection & strings]
-  (binding [*out* (:out connection)]
-    (println (string/join " " strings))))
-
-(defn register [connection nick & [password]]
-  (send-message connection "USER" nick "i *" nick)
-  (send-message connection "NICK" nick)
-  (when password
-    (send-message connection "PRIVMSG nickserv :identify" nick password)))
-
-(defn join-channel
-  "Join a specified channel."
-  [connection channel]
-  (send-message connection "JOIN" channel))
-
-(defn connect [& {:keys [host port nick password channel]}]
-  (future
-    (let [connection (create :host host :port port :nick nick)]
-      (register connection nick password)
-      (join-channel connection channel)
-      (while true
-        (let [next-line (binding [*in* (:in connection)] read-line)]
-          (println next-line))))))
