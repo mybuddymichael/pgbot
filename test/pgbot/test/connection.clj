d09b70a4de215152583e97b1f53d831d5803a0c7
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-17 14:26:52 -0800

    Add several more connection tests

--- test/pgbot/test/connection.clj
+++ test/pgbot/test/connection.clj
@@ -1,8 +1,31 @@
 (ns pgbot.test.connection
-  (:require [clojure.test :refer [deftest is]]
-            [pgbot.connection :as connection]))
+  (:require [clojure.test :refer [use-fixtures deftest is]])
+  (:use pgbot.connection))
 
-(deftest create-returns-a-map-with-socket-in-and-out
-  (let [irc (connection/create "irc.freenode.net" 6665)]
-    (is (= [true true true]
-           (map #(contains? irc %) [:socket :in :out])))))
+(def ^:dynamic *connection*)
+
+(defn handle-connection-fixture [f]
+  (binding [*connection* (create :host "irc.freenode.net" :port 6667 :nick "pgbot")]
+    (f)
+    (.close (*connection* :socket))))
+
+(use-fixtures :once handle-connection-fixture)
+
+(deftest create-returns-a-map-with-socket-in-out-and-nick
+  (is (= [true true true true]
+         (map #(contains? *connection* %) [:socket :in :out :nick]))))
+
+(deftest connection-has-a-socket
+  (is (= java.net.Socket
+         (class (*connection* :socket)))))
+
+(deftest connection-has-a-reader
+  (is (= java.io.BufferedReader
+         (class (*connection* :in)))))
+
+(deftest connection-has-a-writer
+  (is (= java.io.BufferedWriter
+         (class (*connection* :out)))))
+
+(deftest connection-has-a-nick
+  (is (= "pgbot" (*connection* :nick))))
