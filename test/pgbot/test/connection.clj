994db3343bff1ad0134b437101a65c5e1e74dc12
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-17 15:29:34 -0800

    Create a connection var in the test
    This makes it much easier to evaluate code in Vim over using a fixture,
    and it accomplishes the exact same thing, as far as I can tell. So, for
    instance, I can put my cursor inside an `is` form, type `cpp`, and get
    the result of that test immediately. With fixtures the `connection` var
    is not bound to an actual `connection` object, so it fails.

--- test/pgbot/test/connection.clj
+++ test/pgbot/test/connection.clj
@@ -2,30 +2,26 @@
   (:require [clojure.test :refer [use-fixtures deftest is]])
   (:use pgbot.connection))
 
-(def ^:dynamic *connection*)
-
-(defn handle-connection-fixture [f]
-  (binding [*connection* (create :host "irc.freenode.net" :port 6667 :nick "pgbot")]
-    (f)
-    (.close (*connection* :socket))))
-
-(use-fixtures :once handle-connection-fixture)
+(def connection (create "irc.freenode.net" 6667 "pgbot"))
 
 (deftest create-returns-a-map-with-socket-in-out-and-nick
   (is (= [true true true true]
-         (map #(contains? *connection* %) [:socket :in :out :nick]))))
+         (map #(contains? connection %) [:socket :in :out :nick]))))
 
 (deftest connection-has-a-socket
   (is (= java.net.Socket
-         (class (*connection* :socket)))))
+         (class (connection :socket)))))
 
 (deftest connection-has-a-reader
   (is (= java.io.BufferedReader
-         (class (*connection* :in)))))
+         (class (connection :in)))))
 
 (deftest connection-has-a-writer
   (is (= java.io.BufferedWriter
-         (class (*connection* :out)))))
+         (class (connection :out)))))
 
 (deftest connection-has-a-nick
-  (is (= "pgbot" (*connection* :nick))))
+  (is (= "pgbot"
+         (connection :nick))))
+
+(.close (connection :socket))
