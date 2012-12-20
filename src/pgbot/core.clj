e2c9360e21f713ddf47b90845a21d0ebac568990
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-20 09:46:45 -0800

    Make -main no-op

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,9 +1,4 @@
 (ns pgbot.core
   (:require [pgbot.connection :as connection]))
 
-(defn -main [host port nick password channel]
-  (connection/connect :host host
-                      :port port
-                      :nick nick
-                      :password password
-                      :channel channel))
+(defn -main [host port nick password channel])
