c7d7c02cded3caafa064f3ddbb26b53319cb1e45
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-22 14:42:34 -0800

    Use a fixture for io-test

--- test/pgbot/io_test.clj
+++ test/pgbot/io_test.clj
@@ -1,11 +1,17 @@
 (ns pgbot.io-test
-  (:require [clojure.test :refer [deftest is]])
+  (:require [clojure.test :refer [deftest is use-fixtures]])
   (:use pgbot.io)
   (:import java.io.StringWriter))
 
-(defonce connection {:out (StringWriter.)})
+(def ^:dynamic *connection*)
+
+(defn connection-fixture [f]
+  (binding [*connection* {:out (StringWriter.)}]
+    (f)))
+
+(use-fixtures :each connection-fixture)
 
 (deftest send-message-writes-message-to-connection-out
-  (send-message connection "Test string.")
+  (send-message *connection* "Test string.")
   (is (= "Test string.\n"
-         (str (connection :out)))))
+         (str (*connection* :out)))))
