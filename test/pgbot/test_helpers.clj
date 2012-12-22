b9daa4fd57617d7f5a6bd749c520aa4c79d4ca2d
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-22 12:30:52 -0800

    Add deftest-*

--- test/pgbot/test_helpers.clj
+++ test/pgbot/test_helpers.clj
@@ -15,6 +15,18 @@
           (symbol))]
     `(deftest ~test-name ~@body)))
 
+(defmacro deftest-*
+  "Same as deftest*, but creates a private function."
+  [name & body]
+  (let [test-name
+        (-> name
+          (clojure.string/lower-case)
+          (clojure.string/replace #"\W" "-")
+          (clojure.string/replace #"-+" "-")
+          (clojure.string/replace #"-$" "")
+          (symbol))]
+    `(deftest- ~test-name ~@body)))
+
 (deftest- test-pgbot-test-defines-tests
   (let [_ (pgbot-test "this is my test")]
     (is (clojure.test/function? this-is-my-test))))
