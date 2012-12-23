b39ae2baad0d3b7c5b540207c88810ed5f0ccc1f
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-23 07:51:05 -0800

    Don't import java.net.Socket

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -1,12 +1,11 @@
 (ns pgbot.connection
   "A collection of functions for creating and maintaining connections to
-  IRC servers."
-  (:import java.net.Socket))
+  IRC servers.")
 
 (defn- create
   "Create a connection and get a map containing information about it."
   [host port & [nick]]
-  (let [socket (Socket. host (Integer. port))]
+  (let [socket (java.net.Socket. host (Integer. port))]
     {:socket socket
      :in (clojure.java.io/reader socket)
      :out (clojure.java.io/writer socket)
