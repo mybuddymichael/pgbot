632e5ab016d6c79993a13a59b27d7db065920c5f
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-22 20:44:04 -0800

    Update send-message's docstring

--- src/pgbot/io.clj
+++ src/pgbot/io.clj
@@ -2,7 +2,8 @@
   "Functions for handling I/O.")
 
 (defn send-message
-  "Send a message through a connection's writer."
+  "Send a message through a connection's writer. This takes multiple
+  string arguments and will join them with spaces in between."
   [connection message & messages]
   (binding [*out* (connection :out)]
     (println (clojure.string/join " " (cons message messages)))))
