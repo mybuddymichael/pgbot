3495d51d0903f8e21df61413d80fbcb9be302b27
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-23 07:59:26 -0800

    Require pgbot.io in connection

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -1,6 +1,7 @@
 (ns pgbot.connection
   "A collection of functions for creating and maintaining connections to
-  IRC servers.")
+  IRC servers."
+  (:require pgbot.io))
 
 (defn- create
   "Create a connection and get a map containing information about it."
