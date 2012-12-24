3c9c6927001b5ffd56eb600fd36efb230fd5862a
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-24 14:42:30 -0800

    Have create-connection register as well
    It now also takes a single channel name and stores it in the map.

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,15 +1,6 @@
 (ns pgbot.core
   "A simple IRC bot.")
 
-(defn- create-connection
-  "Create a connection and get a map containing information about it."
-  [host port nick]
-  (let [socket (java.net.Socket. host (Integer. port))]
-    {:socket socket
-     :in (clojure.java.io/reader socket)
-     :out (clojure.java.io/writer socket)
-     :nick nick}))
-
 (defn- send-message
   "Send a message through a connection's writer. This takes multiple
   string arguments and will join them with spaces in-between."
@@ -23,6 +14,18 @@
   (let [nick (connection :nick)]
     (send-message connection "NICK" nick)
     (send-message connection "USER" nick "i *" nick)))
+
+(defn- create-connection
+  "Create a connection. Returns a map containing information about it."
+  [host port nick channel]
+  (let [socket (java.net.Socket. host (Integer. port))
+        connection {:socket socket
+                    :in (clojure.java.io/reader socket)
+                    :out (clojure.java.io/writer socket)
+                    :nick nick
+                    :channel channel}]
+    (register-connection connection)
+    connection))
 
 (defn- read-line-from-connection
   "Read a single line from the connection. Returns nil if the socket is
