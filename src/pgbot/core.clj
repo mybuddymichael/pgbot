083b296bd4d0bea537ada721790887dfbfe54b57
Michael Hanson <michael.b.hanson@gmail.com>
2012-12-23 22:45:24 -0800

    Add all the functions to pgbot.core

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,3 +1,25 @@
 (ns pgbot.core
-  (:require [pgbot.connection :as connection]))
+  "A simple IRC bot.")
 
+(defn- create-connection
+  "Create a connection and get a map containing information about it."
+  [host port nick]
+  (let [socket (java.net.Socket. host (Integer. port))]
+    {:socket socket
+     :in (clojure.java.io/reader socket)
+     :out (clojure.java.io/writer socket)
+     :nick nick}))
+
+(defn send-message
+  "Send a message through a connection's writer. This takes multiple
+  string arguments and will join them with spaces in between."
+  [connection message & messages]
+  (binding [*out* (connection :out)]
+    (println (clojure.string/join " " (cons message messages)))))
+
+(defn- register-connection
+  "Send a 'handshake' message to register the connection."
+  [connection]
+  (let [nick (connection :nick)]
+    (send-message connection "NICK" nick)
+    (send-message connection "USER" nick "i *" nick)))
