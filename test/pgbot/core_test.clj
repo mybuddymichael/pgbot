1990cb742bcda3ac7795dac7af4444bb68d552be
Michael Hanson <michael.b.hanson@gmail.com>
2013-01-18 10:47:36 -0800

    Add deftest-* for private tests
    Also added tests for the macros.

--- test/pgbot/core_test.clj
+++ test/pgbot/core_test.clj
@@ -12,12 +12,27 @@
       (clojure.string/replace #"-$" "")
       symbol))
 
-(defmacro ^:private deftest*
+(defmacro deftest*
   "Define a test using deftest, but with a string as the name instead of
    a symbol."
   [name-string & body]
   (let [name-symbol (string->symbol name-string)]
     `(clojure.test/deftest ~name-symbol ~@body)))
+
+(defmacro deftest-*
+  "Define a private test using deftest-, but with a string as the name
+   instead of a symbol."
+  [name-string & body]
+  (let [name-symbol (string->symbol name-string)]
+    `(clojure.test/deftest- ~name-symbol ~@body)))
+
+(clojure.test/deftest- deftest*-creates-a-test-with-a-symbol-name
+  (is (= '(clojure.test/deftest this-is-a-test (clojure.test/is true))
+         (macroexpand-1 `(deftest* "this is a test" (is true))))))
+
+(clojure.test/deftest- deftest-*-creates-a-private-test-with-a-symbol-name
+  (is (= '(clojure.test/deftest- this-is-a-test (clojure.test/is true))
+         (macroexpand-1 `(deftest-* "this is a test" (is true))))))
 
 (defonce connection
   (#'pgbot.core/create-connection "irc.freenode.net" 6667 "pgbot"
