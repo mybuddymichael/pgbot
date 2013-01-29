bc1acccee234e4d54f6789f91868abe5fbeb1491
Michael Hanson <michael.b.hanson@gmail.com>
2013-01-28 20:52:05 -0800

    Change the license to LGPLv3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject pgbot "0.1.0"
   :description "A simple IRC bot, written in Clojure."
-  :license {:name "The MIT License"
-            :url "http://www.opensource.org/licenses/MIT"}
+  :license {:name "LGPLv3"
+            :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone/at-at "1.1.1"]]
   :test-paths ["test" "src/pgbot/plugin"]
