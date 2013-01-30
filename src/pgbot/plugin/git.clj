c6948f3c723cddfa1c48e9b3c53424b92dd181cf
Michael Hanson <michael.b.hanson@gmail.com>
2013-01-30 13:43:36 -0800

    For now git/run returns a list of commit messages

--- src/pgbot/plugin/git.clj
+++ src/pgbot/plugin/git.clj
@@ -3,11 +3,12 @@
   (:require clojure.java.io))
 
 (defn run []
-  (let [git-push-log-maps
-        (->> (file-seq (clojure.java.io/file "/tmp"))
-             (map str)
-             (filter #(re-matches #".*\.edn" %))
-             (map slurp)
-             (map read-string))]))
+  (let [git-push-log-maps (->> (file-seq (clojure.java.io/file "/tmp"))
+                               (map str)
+                               (filter #(re-matches #".*\.edn" %))
+                               (map slurp)
+                               (map read-string))]
+    (->> (map (fn [x] (:messages x)) git-push-log-maps)
+         (flatten))))
 
 (defn parse [line])
