c11f9720dc49811c7321ebb8d327b8ecf1fa352a
Michael Hanson <michael.b.hanson@gmail.com>
2013-02-01 11:50:44 -0800

    Add git/get-git-log-maps

--- src/pgbot/plugin/git.clj
+++ src/pgbot/plugin/git.clj
@@ -2,6 +2,20 @@
   "Watches for git pushes and displays notifications."
   (:require clojure.java.io))
 
+(defn- get-git-log-maps []
+  "Parses /tmp for Git push .edn files, reads them into Clojure maps,
+   then removes the files from the file system. It returns a list of the
+   maps."
+  (let [log-files (->> (file-seq (clojure.java.io/file "/tmp"))
+                       (map str)
+                       (filter #(re-matches #".*irc.*git.*\.edn$" %)))
+        log-maps (->> log-files
+                      (map slurp)
+                      (map read-string)
+                      (doall))]
+    (doseq [f log-files] (clojure.java.io/delete-file (debug f)))
+    log-maps))
+
 (defn run [connection]
   (let [git-push-log-maps (->> (file-seq (clojure.java.io/file "/tmp"))
                                (map str)
