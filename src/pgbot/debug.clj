56ad8fe4fe1381ebc720633bea11b153f10243ce
Michael Hanson <michael.b.hanson@gmail.com>
2013-02-10 10:21:53 -0800

    Change debug/check to debug/inspect

--- src/pgbot/debug.clj
+++ src/pgbot/debug.clj
@@ -1,7 +1,7 @@
 (ns pgbot.debug
   "Simple helpers for debugging.")
 
-(defmacro check [x]
+(defmacro inspect [x]
   "Prints the passed-in symbol plus its value."
   `(let [x# ~x]
      (println "debug:" '~x "=" x#)
