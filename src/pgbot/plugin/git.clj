0831d1b43aee0fbd1b9cefb37eed31088e00b918
Michael Hanson <michael.b.hanson@gmail.com>
2013-02-10 20:56:18 -0800

    Bind *read-eval* to false

--- src/pgbot/plugin/git.clj
+++ src/pgbot/plugin/git.clj
@@ -6,15 +6,16 @@
   "Parses /tmp for Git push .edn files, reads them into Clojure maps,
    then removes the files from the file system. It returns a list of the
    maps."
-  (let [log-files (->> (file-seq (clojure.java.io/file "/tmp"))
-                       (map str)
-                       (filter #(re-matches #".*irc.*git.*\.edn$" %)))
-        log-maps (->> log-files
-                      (map slurp)
-                      (map read-string)
-                      (doall))]
-    (doseq [f log-files] (clojure.java.io/delete-file f))
-    log-maps))
+  (binding [*read-eval* false]
+    (let [log-files (->> (file-seq (clojure.java.io/file "/tmp"))
+                         (map str)
+                         (filter #(re-matches #".*irc.*git.*\.edn$" %)))
+          log-maps (->> log-files
+                        (map slurp)
+                        (map read-string)
+                        (doall))]
+      (doseq [f log-files] (clojure.java.io/delete-file f))
+      log-maps)))
 
 (defn run [connection]
   (let [git-log-maps (get-git-log-maps)
