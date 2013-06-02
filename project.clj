3f7ae494fa2510a1d9a5bf5b3264e5594b558df7
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-01 20:21:59 -0700

    Add ring-jetty-adapter

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name "LGPLv3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.8"]]
   :test-paths ["test" "src/pgbot/plugin"]
   :main pgbot.core)
