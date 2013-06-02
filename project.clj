0f07b448c171d770812f8b7988ddd37c87355b9d
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-02 14:15:44 -0700

    Add clojure.tools.namespace

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.8"]]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [org.clojure/tools.namespace "0.2.3"]]
   :test-paths ["test" "src/pgbot/plugin"]
   :main pgbot.core)
