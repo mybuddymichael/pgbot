d9c53fb8b44b01a31377aee50001e8b5d1d15733
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-03 09:57:22 -0700

    Remove tools.namespace

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.8"]
-                 [org.clojure/tools.namespace "0.2.3"]]
+                 [ring/ring-jetty-adapter "1.1.8"]]
   :test-paths ["test"]
   :main pgbot.core)
