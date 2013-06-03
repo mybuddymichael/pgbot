ef7dda1486d54b23a213d710f335a78aed2aeeca
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-03 09:26:24 -0700

    Add a :port key to start-server

--- src/pgbot/git_listener.clj
+++ src/pgbot/git_listener.clj
@@ -19,4 +19,5 @@
 
 (def application (api app-routes))
 
-(defn start-server [] (run-jetty application {:port 8080 :join? false}))
+(defn start-server [& {:keys [port] :or {port 8080}}]
+  (run-jetty application {:port port :join? false}))
