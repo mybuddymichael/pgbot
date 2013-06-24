7639aff522d12c821f0f48958ad83a831a712f2d
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-23 21:09:50 -0700

    Move output.clj to logger.clj

--- src/pgbot/logger.clj
+++ src/pgbot/logger.clj
@@ -1,5 +1,6 @@
-(ns pgbot.output
-  (:require [pgbot.messages :refer [compose]]))
+(ns pgbot.logger
+  (:require (pgbot [messages :refer [compose]]
+                   [events :as events])))
 
 (defn print-messages
   [_ & messages]
@@ -13,3 +14,6 @@
     (spit "/tmp/pgbot.log"
           (str (java.util.Date.) " : " (compose m) "\n")
           :append true)))
+
+(defn start [_]
+  (events/register [:incoming :outgoing] [print-messages log]))
