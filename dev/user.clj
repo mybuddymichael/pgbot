59a8a2bc26a5f51f1d460bff56ef191601ac8a0c
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-25 20:31:57 -0700

    Add app lifecycle functions to user

--- dev/user.clj
+++ dev/user.clj
@@ -2,3 +2,30 @@
   (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [clojure.repl :refer :all]
             (pgbot system debug)))
+
+(def system nil)
+
+(defn create
+  "Creates and stores a new application instance."
+  []
+  (alter-var-root #'system (constantly (pgbot.system/create))))
+
+(defn start
+  "Starts the current application."
+  []
+  (alter-var-root #'system pgbot.system/start))
+
+(defn stop
+  "Shuts down the application and releases resources."
+  []
+  (alter-var-root #'system (fn [s] (when s (pgbot.system/stop s)))))
+
+(defn go
+  "Creates, stores, and starts a new application."
+  []
+  (create)
+  (start))
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/go))
