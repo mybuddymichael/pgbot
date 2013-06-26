53dadb49a87b4bfffabd8bf372b952658576d9dd
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-26 08:38:32 -0700

    Remove a bad test

--- test/pgbot/core_test.clj
+++ test/pgbot/core_test.clj
@@ -2,9 +2,3 @@
   (:require [clojure.test :use [is]]
             [pgbot.test-helpers :use [deftest* deftest-*]]
             pgbot.core))
-
-(deftest* "events is a map of keywords to fns"
-  (is (= [true true true]
-         (map fn? (#'pgbot.core/events :incoming))))
-  (is (= [true true true]
-         (map fn? (#'pgbot.core/events :outgoing)))))
