7c5b4c54c04af4ee63895dc3c2be7f4d1c7ea1d3
Michael Hanson <michael.b.hanson@gmail.com>
2013-06-27 14:22:07 -0700

    Make commit-server closer to the lifecycle interface

--- src/pgbot/commit_server.clj
+++ src/pgbot/commit_server.clj
@@ -4,19 +4,30 @@
             [compojure.handler :refer [api]]
             [ring.adapter.jetty :refer [run-jetty]]))
 
-(defn start-server [connection & {:keys [port] :or {port 8080}}]
-  (let [app-routes
-        (routes
-          (POST "/" [user-name commit-message repo branch sha]
-                (let [message
-                      (str user-name " in " repo "/" branch ": \""
-                           commit-message"\" (" sha ")")]
-                  (pgbot.events/trigger
-                    connection
-                    :outgoing
-                    {:type "PRIVMSG"
-                     :destination (:channel connection)
-                     :content message}))
-                {:body nil}))
-        handler (api app-routes)]
-    (run-jetty handler {:port (Integer. port) :join? false})))
+(defn- create-handler
+  "Generates a Ring handler to post commits to the connection."
+  [connection]
+  (api (routes
+         (POST "/" [user-name commit-message repo branch sha]
+           (let [message
+                 (str user-name " in " repo "/" branch ": \""
+                      commit-message"\" (" sha ")")]
+             (pgbot.events/trigger
+               connection
+               :outgoing
+               {:type "PRIVMSG"
+                :destination (:channel connection)
+                :content message}))
+           {:body nil}))))
+
+(defn create-and-start
+  "Runs side effects to create and start a new Jetty Server listens for
+   git commits. Returns the new Server."
+  [connection & {:keys [port] :or {port 8080}}]
+  (run-jetty (create-handler connection) {:port (Integer. port) :join? false}))
+
+(defn stop
+  "Runs side effects to stop the commit server."
+  [server]
+  (.stop server)
+  server)
