7bf040dbc303da624f558f6cae36b78eb7391772
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-02 21:18:27 -0700

    Change old system references to application

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,15 +1,15 @@
 (ns pgbot.core
   "A simple IRC bot."
   (:gen-class)
-  (:require (pgbot system)))
+  (:require (pgbot application)))
 
 (defn -main
   "Start pgbot. This will block until the connection closes, at which
    point it will automatically try to reconnect."
   [host port nick channel commit-server-port]
-  (let [system (-> (pgbot.system/create :host host :port port :nick nick
+  (let [application (-> (pgbot.application/create :host host :port port :nick nick
                                         :channel channel
                                         :commit-server-port commit-server-port)
-                   pgbot.system/start)]
-    @(get-in system [:connection :line-loop])
+                   pgbot.application/start)]
+    @(get-in application [:connection :line-loop])
     (recur host port nick channel commit-server-port)))
