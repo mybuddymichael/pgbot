c0b0885a423fb0d39a9beeaf0e3b5d5deb78f725
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-02 21:15:45 -0700

    Have system use the new commit-server functions

--- src/pgbot/system.clj
+++ src/pgbot/system.clj
@@ -11,23 +11,23 @@
            nick "pgbottest"
            channel "##pgbottest"
            commit-server-port 8080}}]
-  {:connection (pgbot.connection/create host port nick channel)
-   :commit-server-port commit-server-port})
+  (let [connection (pgbot.connection/create host port nick channel)]
+    {:connection connection
+     :commit-server (pgbot.commit-server/create commit-server-port
+                                                (connection :out)
+                                                (connection :channel))}))
 
 (defn start
   "Runs various side effects to start up pgbot. Returns the started
    application."
-  [{:keys [connection commit-server-port] :as system}]
+  [{:keys [connection commit-server] :as system}]
   (let [connection
         (-> connection
             pgbot.logger/start
             pgbot.connection/start)]
     (assoc system
            :connection connection
-           :commit-server
-           (pgbot.commit-server/create-and-start connection
-                                                 :port
-                                                 commit-server-port))))
+           :commit-server (pgbot.commit-server/start commit-server))))
 
 (defn stop
   "Runs various side effects to shut down pgbot. Returns the stopped
