728f13215626395274bd0cefd749ef2600b2d295
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-06 23:17:50 -0700

    Add dispatcher to application's functions

--- src/pgbot/application.clj
+++ src/pgbot/application.clj
@@ -1,8 +1,9 @@
 (ns pgbot.application
   (:require (pgbot connection
-                   logger
                    commit-server
-                   ping-pong)
+                   ping-pong
+                   dispatcher
+                   logger)
             [clojure.core.async :refer [chan]]))
 
 (defn create
@@ -11,11 +12,14 @@
   (let [port (Integer. port)
         commit-server-port (Integer. commit-server-port)
         {:keys [out channel] :as connection} (pgbot.connection/create
-                                               host port nick channel)]
+                                               host port nick channel)
+        ping-pong (pgbot.ping-pong/create out)
+        commit-server (pgbot.commit-server/create
+                        commit-server-port out channel)]
     {:connection connection
-     :ping-pong (pgbot.ping-pong/create out)
-     :commit-server (pgbot.commit-server/create
-                      commit-server-port out channel)}))
+     :ping-pong ping-pong
+     :commit-server commit-server
+     :dispatcher (pgbot.dispatcher/create [(ping-pong :in)])}))
 
 (defn create-dev
   "Creates and returns a new pgbot instance suitable for development."
@@ -29,7 +33,7 @@
 (defn start
   "Runs various side effects to start up pgbot. Returns the started
    application."
-  [{:keys [connection commit-server ping-pong] :as application}]
+  [{:keys [connection commit-server ping-pong dispatcher] :as application}]
   (let [connection
         (-> connection
             pgbot.logger/start
@@ -37,13 +41,15 @@
     (assoc application
            :connection connection
            :commit-server (pgbot.commit-server/start commit-server)
-           :ping-pong (pgbot.ping-pong/stop ping-pong))))
+           :ping-pong (pgbot.ping-pong/stop ping-pong)
+           :dispatcher (pgbot.dispatcher/start dispatcher))))
 
 (defn stop
   "Runs various side effects to shut down pgbot. Returns the stopped
    application."
-  [{:keys [connection commit-server ping-pong] :as application}]
+  [{:keys [connection commit-server ping-pong dispatcher] :as application}]
   (assoc application
          :commit-server (pgbot.commit-server/stop commit-server)
          :connection (pgbot.connection/stop connection)
-         :ping-pong (pgbot.ping-pong/stop ping-pong)))
+         :ping-pong (pgbot.ping-pong/stop ping-pong)
+         :dispater (pgbot.dispatcher/stop dispatcher)))
