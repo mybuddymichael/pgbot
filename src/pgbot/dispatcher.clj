dfce36f3982f945bb066e15f596a05f2b2eb7b7d
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-06 20:42:34 -0700

    Implement dispatcher

--- src/pgbot/dispatcher.clj
+++ src/pgbot/dispatcher.clj
@@ -1,2 +1,20 @@
 (ns pgbot.dispatcher
-  (require [clojure.core.async :refer [go <! >!]]))
+  (require [clojure.core.async :refer [chan go <! >! >!!]]))
+
+(defn create [in channels]
+  {:in in
+   :channels channels
+   :stop (chan)})
+
+(defn start [{:keys [in channels stop] :as dispatcher}]
+  (go
+    (loop [[message channel] (alts! [stop in] :priority true)]
+      (when (not= channel stop)
+        (doseq [c channels]
+          (go (>! c message)))
+        (recur (alts! [stop in] :priority true)))))
+  dispatcher)
+
+(defn stop [{:keys [stop] :as dispatcher}]
+  (>!! stop true)
+  dispatcher)
