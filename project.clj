65993152918b38a8ae129c12b2548e0aa6cd4e7e
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-11 07:58:25 -0700

    pgbot 0.4.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pgbot "0.4.4"
+(defproject pgbot "0.4.5"
   :description "A simple IRC bot, written in Clojure."
   :license {:name "LGPLv3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
