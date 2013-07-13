daf311cf2123eb69f4a682215e1ef8dc53bb3bd9
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-12 22:25:18 -0700

    Update connection/start's destructuring

--- src/pgbot/connection.clj
+++ src/pgbot/connection.clj
@@ -39,7 +39,7 @@
    establish a connection it will keep trying until it succeeds. It
    starts placing incoming messages into the in channel and taking
    outgoing message off the out channel."
-  [{:keys [host port nick channel in out] :as connection}]
+  [{:keys [host port nick channel in-chans out-chans stop] :as connection}]
   (let [open-socket
         (fn [host port]
           (or (try (java.net.Socket. host port)
