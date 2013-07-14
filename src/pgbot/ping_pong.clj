75e433571bd4fdd531060a645dd19fdda71ddda4
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-14 14:12:25 -0700

    Convert PingPong to a record

--- src/pgbot/ping_pong.clj
+++ src/pgbot/ping_pong.clj
@@ -1,30 +1,22 @@
 (ns pgbot.ping-pong
-  (:require [clojure.core.async :refer [chan go <! >! close! alts!]]))
+  (:require (pgbot [lifecycle :refer [Lifecycle]])
+            [clojure.core.async :refer [chan go <! >! close! alts!]]))
 
-(defn create
-  "Creates and returns a system to check for ping messages from the IRC
-   server."
-  [out]
-  {:in (chan)
-   :out out
-   :stop (chan)})
+(defrecord PingPong [in out stop])
 
-(defn start
-  "Runs side effects to begin listening for ping-pong messages inside a
-   go process. Returns the system."
-  [{:keys [in out stop] :as ping-pong}]
-  (go
-    (loop [[message chan] (alts! [stop in] :priority true)]
-      (when (and (not= chan stop)
-                 (= (message :type) "PING"))
-        (>! out {:type "PONG"
-                 :content (message :content)})
-        (recur (alts! [stop in] :priority true)))))
-  ping-pong)
+(extend-type PingPong
+  Lifecycle
+  (start [{:keys [in out stop] :as ping-pong}]
+    (go
+      (loop [[message chan] (alts! [stop in] :priority true)]
+        (when (and (not= chan stop)
+                   (= (message :type) "PING"))
+          (>! out {:type "PONG"
+                   :content (message :content)})
+          (recur (alts! [stop in] :priority true)))))
+    ping-pong)
+  (stop [{:keys [stop] :as ping-pong}]
+    (close! stop)
+    ping-pong))
 
-(defn stop
-  "Runs side effects to stop the ping-pong system. Returns the stopped
-   system."
-  [{:keys [stop] :as ping-pong}]
-  (close! stop)
-  ping-pong)
+(defn ->PingPong [] (PingPong. (chan) (chan) (chan)))
