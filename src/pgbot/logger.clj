32d9b334d6a1cd49beb9fb10d929b362d4fa6a9f
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-14 21:01:48 -0700

    Have Logger listen to in and recur correctly

--- src/pgbot/logger.clj
+++ src/pgbot/logger.clj
@@ -3,22 +3,23 @@
                    [messages :refer [compose]])
             [clojure.core.async :refer [alts! chan go <! close!]]))
 
-(defrecord Logger [out-listener stop])
+(defrecord Logger [in out-listener stop])
 
 (extend-type Logger
   Lifecycle
-  (start [{:keys [out-listener stop] :as Logger}]
-    (go (loop [[message chan] (alts! [stop out-listener] :priority true)]
-          (when (not= chan stop)
-            (println (compose message))
-            (spit "/tmp/pgbot.log"
-                  (str (java.util.Date.) " : " (compose message) "\n")
-                  :append true))
-          (recur (alts! [stop out-listener] :priority true))))
+  (start [{:keys [in out-listener stop] :as Logger}]
+    (go
+      (loop [[message chan] (alts! [stop in out-listener] :priority true)]
+        (when (not= chan stop)
+          (println (compose message))
+          (spit "/tmp/pgbot.log"
+                (str (java.util.Date.) " : " (compose message) "\n")
+                :append true)
+          (recur (alts! [stop in out-listener] :priority true)))))
     Logger)
   (stop [{:keys [stop] :as Logger}]
     (close! stop)
     Logger))
 
 (defn ->Logger []
-  (Logger. (chan) (chan)))
+  (Logger. (chan) (chan) (chan)))
