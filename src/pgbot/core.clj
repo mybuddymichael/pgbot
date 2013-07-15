ccd914c90c09bea26fa6cae4b7db929fb308d2c9
Michael Hanson <michael.b.hanson@gmail.com>
2013-07-14 21:15:12 -0700

    Update pgbot.core for the new interfaces

--- src/pgbot/core.clj
+++ src/pgbot/core.clj
@@ -1,15 +1,16 @@
 (ns pgbot.core
   "A simple IRC bot."
   (:gen-class)
-  (:require (pgbot application)))
+  (:require (pgbot application)
+            [clojure.core.async :refer [<!!]]))
 
 (defn -main
   "Start pgbot. This will block until the connection closes, at which
    point it will automatically try to reconnect."
   [host port nick channel commit-server-port]
-  (let [application (-> (pgbot.application/create :host host :port port :nick nick
-                                        :channel channel
-                                        :commit-server-port commit-server-port)
-                   pgbot.application/start)]
-    @(get-in application [:connection :line-loop])
+  (let [application (-> (pgbot.application/create
+                          {:host host :port port :nick nick :channel channel
+                           :commit-server-port commit-server-port})
+                        pgbot.application/start)]
+    (<!! (get-in application [:connection :in-loop]))
     (recur host port nick channel commit-server-port)))
