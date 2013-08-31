df6673df11ce496786532705eeef13de7e8cfc78
Michael Hanson <michael.b.hanson@gmail.com>
2013-08-31 15:15:59 -0700

    Add static typing to pgbot.logger
    The stop channel is now called kill to avoid overlap with the stop
    function that closes it.

--- src/pgbot/logger.clj
+++ src/pgbot/logger.clj
@@ -1,24 +1,34 @@
 (ns pgbot.logger
   (:require (pgbot [process :refer [PProcess]]
                    [messages :refer [compose]])
-            [clojure.core.async :refer [alts! chan go <! close!]]))
+            [clojure.core.typed :as t]
+            [clojure.core.typed.async :as a :refer [Chan chan> go>]]
+            [clojure.core.async :refer [alts! go <! close!]]))
 
-(defrecord Logger [in out-listener stop])
+(t/typed-deps clojure.core.typed.async)
+
+(t/ann-record Logger [in := (Chan pgbot.messages.Message)
+                      out-listener := (Chan pgbot.messages.Message)
+                      kill := (Chan Any)])
+
+(defrecord Logger [in out-listener kill])
 
 (extend-type Logger
   PProcess
-  (start [{:keys [in out-listener stop] :as logger}]
-    (go
-      (loop [[message chan] (alts! [stop in out-listener] :priority true)]
-        (when (not= chan stop)
+  (start [{:keys [in out-listener kill] :as logger}]
+    (go>
+      (loop [[message chan] (alts! [kill in out-listener] :priority true)]
+        (when (not= chan kill)
           (spit "/tmp/pgbot.log"
                 (str (java.util.Date.) " : " (compose message) "\n")
                 :append true)
-          (recur (alts! [stop in out-listener] :priority true)))))
+          (recur (alts! [kill in out-listener] :priority true)))))
     logger)
-  (stop [{:keys [stop] :as logger}]
-    (close! stop)
+  (stop [{:keys [kill] :as logger}]
+    (close! kill)
     logger))
 
 (defn ->Logger []
-  (Logger. (chan) (chan) (chan)))
+  (Logger. (chan> pgbot.messages.Message)
+           (chan> pgbot.messages.Message)
+           (chan> Any)))
