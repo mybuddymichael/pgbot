28047e2679936793161aa7aa4d9051dbb4945349
Michael Hanson <michael.b.hanson@gmail.com>
2013-08-31 15:20:29 -0700

    Change Process to PProcess and fix the function names

--- src/pgbot/process.clj
+++ src/pgbot/process.clj
@@ -1,10 +1,10 @@
 (ns pgbot.process
   (:require [clojure.core.typed :as t]))
 
-(t/ann-protocol Process
-                start [Process -> Process]
-                start [Process -> Process])
-(t/defprotocol> Process
+(t/ann-protocol PProcess
+                start [PProcess -> PProcess]
+                stop [PProcess -> PProcess])
+(t/defprotocol> PProcess
   "A protocol for starting and stopping processes."
   (start [system] "Runs side effects to start the process. Returns the process.")
   (stop [system] "Runs side effects to stop the process. Returns the process."))
