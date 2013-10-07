369e2249602660f71036706f81d877b4d84d9006
Michael Hanson <michael.b.hanson@gmail.com>
2013-10-07 13:17:22 -0700

    Add annotations to commit-server

--- src/pgbot/commit_server.clj
+++ src/pgbot/commit_server.clj
@@ -1,19 +1,24 @@
 (ns pgbot.commit-server
   (:require (pgbot [process :refer [PProcess]])
             [clojure.core.async :refer [chan go put!]]
+            [clojure.core.typed :as t]
+            [clojure.core.typed.async :refer [Chan]]
             [compojure.core :refer [routes POST]]
             [compojure.handler :refer [api]]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:import org.eclipse.jetty.server.Server))
 
+(t/ann-record CommitServer [server :- Server
+                            out :- (Chan Any)])
 (defrecord CommitServer [server out])
 
 (extend-type CommitServer
   PProcess
   (start [{:keys [server] :as commit-server}]
-    (.start server)
+    (.start ^Server server)
     commit-server)
   (stop [{:keys [server] :as commit-server}]
-    (.stop server)
+    (.stop ^Server server)
     commit-server))
 
 (defn ->CommitServer
