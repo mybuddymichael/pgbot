96a6991f8ebd1511b819f97dbe00ed983ad94c75
Michael Hanson <michael.b.hanson@gmail.com>
2013-10-07 09:32:09 -0700

    Move core.typed to a dev dep only

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.8"]
-                 [org.clojure/core.typed "0.2.1"]]
+                 [ring/ring-jetty-adapter "1.1.8"] ]
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
   :main pgbot.core
   :profiles {:dev {:source-paths ["dev"]
                    :repl-options {:init-ns user}
-                   :dependencies [[org.clojure/tools.namespace "0.2.3"]]}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                                  [org.clojure/core.typed "0.2.13"]]}})
