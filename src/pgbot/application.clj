5341ba111b155b317a321bac053e2fd37dc6f54d
Michael Hanson <michael.b.hanson@gmail.com>
2013-11-21 15:19:32 -0800

    Start and stop the dispatcher from application

--- src/pgbot/application.clj
+++ src/pgbot/application.clj
@@ -1,17 +1,23 @@
 (ns pgbot.application
-  (:require (pgbot [lifecycle :as lifecycle :refer [Lifecycle]]
-                   [connection :refer [Connection]]
-                   commit-server
-                   responder)
-            [clojure.core.typed :as t :refer [ann def-alias Vec]])
+  (:require [clojure.core.async :as async]
+            [clojure.core.typed :as t :refer [ann def-alias Nilable Seqable]]
+            [clojure.core.typed.async :refer [Chan]]
+            (pgbot [lifecycle :as lifecycle :refer [Lifecycle]]
+                   [commit-server :as commit-server]
+                   [connection :as connection :refer [Connection]]
+                   [dispatcher :as dispatcher]
+                   [messages :refer [Message]]
+                   [responder :as responder]))
   (:import (clojure.lang Keyword)
-           pgbot.responder.Responder
-           pgbot.commit_server.CommitServer))
+           pgbot.commit_server.CommitServer
+           pgbot.dispatcher.Dispatcher
+           pgbot.responder.Responder))
 
 (def-alias Application
   (HMap :mandatory {:connection Connection
                     :responder Responder
-                    :commit-server CommitServer}))
+                    :commit-server CommitServer
+                    :dispatcher Dispatcher}))
 
 (ann create [(HMap :mandatory {:host String
                                :port String
@@ -22,36 +28,42 @@
 (defn create
   "Creates and returns a new instance of pgbot."
   [{:keys [host port nick channel commit-server-port]}]
-  (let [port (Integer. ^String port)
-        commit-server-port (Integer. ^String commit-server-port)
-        commit-server (pgbot.commit-server/->CommitServer commit-server-port
-                                                          channel)
-        responder (pgbot.responder/->Responder)
+  (let [port (Integer/parseInt port)
+        commit-server-port (Integer/parseInt commit-server-port)
+        commit-server (commit-server/->CommitServer commit-server-port
+                                                    channel)
+        responder (responder/->Responder)
         in-chans [(:in responder)]
         out-chans [(:out responder) (:out commit-server)]
-        connection (pgbot.connection/create host port nick channel
-                                            in-chans out-chans)]
+        connection (connection/create host port nick channel)
+        dispatcher (dispatcher/map->Dispatcher {:incoming (:in connection)
+                                                :outgoing (:out connection)
+                                                :in-chans in-chans
+                                                :out-chans out-chans})]
     {:connection connection
      :responder responder
-     :commit-server commit-server}))
-
+     :commit-server commit-server
+     :dispatcher dispatcher}))
 
 (ann ^:no-check start [Application -> Application])
 (defn start
   "Runs various side effects to start up pgbot. Returns the started
    application."
-  [{:keys [connection responder commit-server] :as application}]
+  [{:keys [connection responder commit-server dispatcher]
+    :as application}]
   (assoc application
          :connection (pgbot.connection/start connection)
          :responder (lifecycle/start responder)
-         :commit-server (lifecycle/start commit-server)))
+         :commit-server (lifecycle/start commit-server)
+         :dispatcher (lifecycle/start dispatcher)))
 
 (ann ^:no-check stop [Application -> Application])
 (defn stop
   "Runs various side effects to shut down pgbot. Returns the stopped
    application."
-  [{:keys [connection responder commit-server] :as application}]
+  [{:keys [connection responder commit-server dispatcher] :as application}]
   (assoc application
          :connection (pgbot.connection/stop connection)
          :responder (lifecycle/stop responder)
-         :commit-server (lifecycle/stop commit-server)))
+         :commit-server (lifecycle/stop commit-server)
+         :dispatcher (lifecycle/stop dispatcher)))
