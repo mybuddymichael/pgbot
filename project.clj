12048e302ccebd150220ff7548acab34a671bef0
Michael Hanson <michael.b.hanson@gmail.com>
2013-11-21 20:49:51 -0800

    Add timbre as a dep for logging

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.1.8"]
-                 [org.clojure/core.typed "0.2.19"]]
+                 [org.clojure/core.typed "0.2.19"]
+                 [com.taoensso/timbre "2.7.1"]]
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
   :main pgbot.core
