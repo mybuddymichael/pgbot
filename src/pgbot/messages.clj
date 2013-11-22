d7f2a1cf3ca58811ed061d69def0a9a7d9bc1993
Michael Hanson <michael.b.hanson@gmail.com>
2013-11-21 20:50:57 -0800

    Add a UUID to parsed messages

--- src/pgbot/messages.clj
+++ src/pgbot/messages.clj
@@ -1,23 +1,28 @@
 (ns pgbot.messages
   "Functions for parsing and composing IRC lines."
-  (:require [clojure.core.typed :as t]))
+  (:require [clojure.core.typed :as t]
+            [taoensso.timbre :refer [info]])
+  (:import java.util.UUID))
 
 (t/def-alias Message
   (HMap :mandatory {:type (U String nil)
                     :destination (U String nil)}
         :optional {:prefix (U String nil)
-                   :content (U String nil)}))
+                   :content (U String nil)
+                   :uuid UUID}))
 
 (t/ann parse [String -> Message])
 (defn parse
   "Takes a line and returns a Message."
   [line]
   (let [[[_ prefix type destination content]]
-        (re-seq #"^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$" line)]
-    {:prefix (some-> prefix str)
-     :type (some-> type str)
-     :destination (some-> destination str)
-     :content (some-> content str)}))
+        (re-seq #"^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$" line)
+        message {:prefix (some-> prefix str)
+                 :type (some-> type str)
+                 :destination (some-> destination str)
+                 :content (some-> content str)
+                 :uuid (UUID/randomUUID)}]
+    message))
 
 (t/ann compose [Message -> String])
 (defn compose
