e1244c81c85902a2f56d1f60cb0d8f9f03f2073e
Michael Hanson <michael.b.hanson@gmail.com>
2013-12-26 14:55:53 -0800

    Record messages to Datomic

--- src/pgbot/recorder.clj
+++ src/pgbot/recorder.clj
@@ -1,7 +1,7 @@
 (ns pgbot.recorder
   (:require [clojure.core.async :as async]
             [datomic.api :as d]
-            [taoensso.timbre :refer [info]]
+            [taoensso.timbre :refer [error info]]
             [pgbot.lifecycle :refer [Lifecycle]]))
 
 (defrecord Recorder [db-conn in]
@@ -11,7 +11,18 @@
       (loop [message (async/<!! in)]
         (when message
           (info "Recording message" (hash message))
-          (recur (async/<!! in)))))
+          (let [transaction
+                [{:db/id #db/id [:db.part/user]
+                  :message/prefix (str (message :prefix))
+                  :message/user (str (message :user))
+                  :message/uri (str (message :uri))
+                  :message/type (str (message :type))
+                  :message/destination (str (message :destination))
+                  :message/content (str (message :content))}]]
+            (try @(d/transact db-conn transaction))
+              (catch Exception e (error e))
+            (info "Message" (hash message) "recorded.")
+            (recur (async/<!! in))))))
     (info "Recorder started.")
     recorder)
   (stop [recorder]
